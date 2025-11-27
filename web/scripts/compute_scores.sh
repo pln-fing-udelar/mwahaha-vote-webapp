bb3d204112a453086ca1a4c3ecdfa3b322e11b38
Santiago Castro <santiagoc@netflix.com>
2025-11-27 02:27:20 -0300

    Make a message clearer

--- web/scripts/compute_scores.sh
+++ web/scripts/compute_scores.sh
@@ -7,7 +7,7 @@ uv run --env-file ../.env scripts/prepare_data_for_scoring.py
 for task in a-en a-es a-zh b1 b2; do
   json_file=scoring/votes-$task.json
   if (( $(wc -c < "$json_file") <= 2 )); then
-    echo "Skipping $task as it doesn't have votes"
+    echo "Skipping the unvoted task $task."
   else
     uv run -m fastchat.serve.monitor.elo_analysis --clean-battle-file "$json_file" --num-cpu 1
     mv elo_results_*.pkl "scoring/elo_results_$task.pkl"
