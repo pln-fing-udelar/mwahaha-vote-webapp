bfa7bc12397634a4ac823e0e6f7d317739c5cb2f
Santiago Castro <santiagoc@netflix.com>
2025-11-27 02:25:57 -0300

    Skip the empty files in compute_scores.sh

--- web/scripts/compute_scores.sh
+++ web/scripts/compute_scores.sh
@@ -5,6 +5,11 @@ set -x
 uv run --env-file ../.env scripts/prepare_data_for_scoring.py
 
 for task in a-en a-es a-zh b1 b2; do
-  uv run -m fastchat.serve.monitor.elo_analysis --clean-battle-file scoring/votes-${task}.json
-  mv elo_results_*.pkl scoring/elo_results_${task}.pkl
+  json_file=scoring/votes-$task.json
+  if (( $(wc -c < "$json_file") <= 2 )); then
+    echo "Skipping $task as it doesn't have votes"
+  else
+    uv run -m fastchat.serve.monitor.elo_analysis --clean-battle-file "$json_file" --num-cpu 1
+    mv elo_results_*.pkl "scoring/elo_results_$task.pkl"
+  fi
 done
