bcf1b07f974579a52e03fa9bc7ebda0e4a4b2342
Santiago Castro <santi.1410@hotmail.com>
2026-02-05 22:04:24 -0800

    For Prolific sessions, also use the SESSION_ID field as part of our session ID

--- web/src/mwahahavote/__main__.py
+++ web/src/mwahahavote/__main__.py
@@ -39,8 +39,8 @@ def _generate_id() -> str:  # From https://stackoverflow.com/a/2257449/1165181
 
 
 def _get_session_id() -> str:
-    if prolific_id := request.args.get("PROLIFIC_PID"):
-        return f"prolific-id-{prolific_id}"
+    if (prolific_id := request.args.get("PROLIFIC_PID")) and (session_id := request.args.get("SESSION_ID")):
+        return f"prolific-id-{prolific_id}-{session_id}"
     else:
         return request.cookies.get("id") or _generate_id()
 
