88d6bf4a844fdbf7f6e614fcd5221ed92d8902cc
Santiago Castro <santi.1410@hotmail.com>
2026-02-08 15:38:08 -0800

    Ignore if the removed file doesn't exist in compute_scores.sh

--- web/scripts/compute_scores.sh
+++ web/scripts/compute_scores.sh
@@ -8,7 +8,7 @@ for task in a-en a-es a-zh b1 b2; do
   json_file=scoring/votes-$task.json
   if (( $(wc -c < "$json_file") <= 2 )); then
     echo "Skipping the unvoted task $task."
-    rm "scoring/elo_results_$task.pkl"
+    rm -f "scoring/elo_results_$task.pkl"
   else
     uv run -m fastchat.serve.monitor.elo_analysis --clean-battle-file "$json_file" --num-cpu 1
     mv elo_results_*.pkl "scoring/elo_results_$task.pkl"
